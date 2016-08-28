565120106905f46c85c5c54bbfd412248fc7f04d
Tagliatti <filipetagliatti@yahoo.com.br>
2016-08-28 06:40:38 +0000

    .

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var boletoValidator = require('../index');
 var assert = require('assert');
 
-descrive('Boleto Validator', function () {
+describe('Boleto Validator', function () {
     it ('Convenio valid module 10', function() {
         assert.ok(boletoValidator.convenio('83640000001-1 33120138000-2 81288462711-6 08013618155-1'));
     });
@@ -18,55 +18,47 @@ descrive('Boleto Validator', function () {
     });
 
     if ('Convenio invalid module 10', function() {
-        assert.ok(boletoValidator.convenio('83640000001-2 33120138000-2 81288462711-6 08013618155-1'));
+        assert.equal(boletoValidator.convenio('83640000001-2 33120138000-2 81288462711-6 08013618155-1'), false);
     });
 
     it ('Convenio invalid module11', function() {
-        assert.ok(boletoValidator.convenio('85890000460-8 52460179160-5 60759305086-5 83148300001-0'));
+        assert.equal(boletoValidator.convenio('85890000460-8 52460179160-5 60759305086-5 83148300001-0'), false);
     });
 
     /**
      * 1 caracter a mais
-     *
-     * @expectedException Exception
      */
-    function testConvenioInvalidFormat1() {
-        assert.ok(boletoValidator.convenio('85890000460-9 52460179160-5 60759305086-5 83148300001-09');
-    }
+    it ('Convenio invalid format 1', function() {
+        assert.throws(boletoValidator.convenio.bind(null, '85890000460-9 52460179160-5 60759305086-5 83148300001-09'));
+    });
 
     /**
      * Passando uma letra
-     *
-     * @expectedException Exception
      */
-    function testConvenioInvalidFormat2() {
-        assert.ok(boletoValidator.convenio('85890000460-9 52460179160-5 60759305086-5 83148300001-a');
-    }
+    it ('Convenio invalid format 2', function() {
+        assert.throws(boletoValidator.convenio.bind(null, '85890000460-9 52460179160-5 60759305086-5 83148300001-a'));
+    });
 
-    function testBoletoValid() {
+    it ('Boleto valid', function() {
         assert.ok(boletoValidator.boleto('42297.11504 00001.954411 60020.034520 2 68610000054659'));
-    }
+    });
 
-    function testBoletoInvalid() {
-        assert.ok(boletoValidator.boleto('92297.11504 00001.954411 60020.034520 2 68610000054659'));
-    }
+    it ('Boleto invalid', function() {
+        assert.equal(boletoValidator.boleto('92297.11504 00001.954411 60020.034520 2 68610000054659'), false);
+    });
 
 
     /**
      * 1 caracter a mais
-     *
-     * @expectedException Exception
      */
-    function testBoletoInvalidFormat1() {
-        assert.ok(boletoValidator.boleto('42297.11504 00001.954411 60020.034520 2 686100000546590');
-    }
+    it ('Boleto invalid format 1', function() {
+        assert.throws(boletoValidator.boleto.bind(null, '42297.11504 00001.954411 60020.034520 2 686100000546590'));
+    });
 
     /**
      * Passando uma letra
-     *
-     * @expectedException Exception
      */
-    function testBoletoInvalidFormat2() {
-        assert.ok(boletoValidator.boleto('42297.11504 00001.954411 60020.034520 2 6861000005465a');
-    }
+    it ('Boleto invalid format 2', function() {
+        assert.throws(boletoValidator.boleto.bind(null, '42297.11504 00001.954411 60020.034520 2 6861000005465a'));
+    });
 });
